9b8f281a5ab9d7342a41ef0f902fd998a385dd26
Matt <msdedwards@hachisoft.com>
2015-02-22 10:04:33 -0800

    initial commi

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name validatorgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the validatorgithubioApp
+ */
+angular.module('validatorgithubioApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
